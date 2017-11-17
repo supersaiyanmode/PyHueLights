072d457ade4a482eee4ca2a6d51478c67f217239
Srivatsan Iyer <supersaiyanmode.rox@gmail.com>
2017-11-16 18:09:58 -0800

    Adding RequestFailed exception.

--- highlight/exceptions.py
+++ highlight/exceptions.py
@@ -12,3 +12,13 @@ class DiscoveryFailed(HighlightException):
     """ Unable to discover the Hue Bridge. """
     def __init__(self):
         super(DiscoveryFailed, self).__init__("No Hue bridges found.")
+
+
+class RequestFailed(HighlightException):
+    """
+    Raised for all exceptions stemming from failure in requesting info from
+    the bridge.
+    """
+    def __init__(self, response):
+        super(RequestFailed, self).__init__("Request to bridge failed.")
+        self.response = response
