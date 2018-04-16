ca0fdd743fd6c572379cf9281a44caa4ed187dfc
Srivatsan Iyer <supersaiyanmode.rox@gmail.com>
2018-04-16 13:27:54 -0700

    Extract get, put, post as separate mixins.

--- highlight/core.py
+++ highlight/core.py
@@ -41,20 +41,26 @@ class HueConnectionInfo(object):
         return True
 
 
+class Get(object):
+    def get(self, relative_url=None, **kwargs):
+        return self.make_request('get', relative_url=relative_url, **kwargs)
+
+
+class Post(object):
+    def post(self, obj, relative_url=None, **kwargs):
+        return self.make_request('post', relative_url=relative_url, **kwargs)
+
+
+class Put(object):
+    def put(self, obj, relative_url=None, **kwargs):
+        return self.make_request('put', relative_url=relative_url, **kwargs)
+
+
 class HueResource(object):
     def __init__(self, connection_info, parent=None):
         self.connection_info = connection_info
         self.parent = parent
         self.dirty_flag = {}
-
-    def get(self, relative_url=None):
-        return self.make_request('get')
-
-    def put(self, params, relative_url=None):
-        return self.make_request('put')
-
-    def delete(self, relative_url=None):
-        return self.make_request('delete', relative_url=relative_url)
 
     def make_request(self, method, **kwargs):
         expected_status = kwargs.pop('expected_status', [])
