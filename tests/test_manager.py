95c594b87dfec24b952d8b0fbfd9c9b97f8dcdeb
Srivatsan Iyer <supersaiyanmode.rox@gmail.com>
2018-04-19 23:35:01 -0700

    Update tests.

--- tests/test_manager.py
+++ tests/test_manager.py
@@ -2,19 +2,25 @@ from highlight.manager import construct_body
 
 from utils import CustomResourceTestBase
 
+
+class TestDictParser(CustomResourceTestBase):
+    pass
+
+
 class TestConstructBody(CustomResourceTestBase):
     def test_construct_body(self):
         resource = self.get_resource(self.obj)
 
-        resource.field2 = "new_value"
         resource.field3.sub2.test = 5
 
         res = construct_body(resource)
         expected = {
-            "f2": "new_value",
             "field3": {
                 "sub2": {"test": 5}
             }
         }
 
         assert res == expected
+
+    def test_construct_body_none(self):
+        assert construct_body(None) is None
