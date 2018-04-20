0cf052a696c266ec70725beb301f10bda3cd4648
Srivatsan Iyer <supersaiyanmode.rox@gmail.com>
2018-04-19 22:17:12 -0700

    Adding tests.

--- tests/test_core.py
+++ tests/test_core.py
@@ -8,7 +8,7 @@ from highlight.manager import update_from_object, construct_body
 class TestUpdateFromObject(object):
     class SubSubResource(HueResource):
         FIELDS = [
-            {"name": "test"}
+            {"name": "test", "values": [1, 2, 3, 4, 5]}
         ]
 
         def relative_url(self):
@@ -87,6 +87,10 @@ class TestUpdateFromObject(object):
         with pytest.raises(ValueError):
             update_from_object(resource, "id", obj)
 
+    def test_update_from_object_without_field(self):
+        with pytest.raises(ValueError):
+            update_from_object(object(), "", None)
+
     def test_property_update(self):
         obj = {
             "field1": "blah",
@@ -114,6 +118,24 @@ class TestUpdateFromObject(object):
         assert resource.dirty_flag == {"field2": True, "field3": True}
         assert resource.field3.dirty_flag == {"sub2": True}
         assert resource.field3.sub2.dirty_flag == {"test": True}
+
+    def test_property_update_invalid_value(self):
+        obj = {
+            "field1": "blah",
+            "f2": "hello",
+            "field3": {
+                "sub": "subval",
+                "sub2": {"test": 1}
+            }
+        }
+
+        resource = self.CustomResource()
+        update_from_object(resource, "id", obj)
+
+        resource.field3.sub2.test = 4
+
+        with pytest.raises(ValueError):
+            resource.field3.sub2.test = 10  # [1,5] are valid values.
 
     def test_sub_resource_relative_url(self):
         obj = {
